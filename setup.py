97a9c2e80ef4fece131ed37c971014f526aafb20
Alexander Rose <alexander.rose@weirdbyte.de>
2016-01-01 19:28:39 -0800

    CODE: added package data and info to setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,40 @@
 
 from setuptools import setup
 
+
+VERSION = "0.1dev"
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: JavaScript",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Chemistry",
+    "Topic :: Scientific/Engineering :: Visualization",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+]
+
+
 if __name__ == '__main__':
     setup(
         name = "MDsrv",
-        version = "0.1dev",
         author = "Alexander S. Rose",
+        author_email = "alexander.rose@weirdbyte.de",
         description = "Server for coordinate trajectories from molecular dynamics simulations",
-        py_modules = [ 'mdsrv' ],
+        version = VERSION,
+        classifiers = CLASSIFIERS,
         license = "MIT",
         url = "https://github.com/arose/mdsrv",
         zip_safe = False,
+        include_package_data = True,  # use MANIFEST.in during install
+        py_modules = [ "mdsrv" ],
         install_requires = [ "Flask", "simpletraj" ],
         entry_points = {
             "console_scripts": [
