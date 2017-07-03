674fff6e32b2706eac4740d06e866cb43f5912c9
j0kaso <johanna.tiemann@gmail.com>
2017-07-03 05:23:01 +0200

    NEW: layout & css style (http://www.css3templates.co.uk/) CHANGE: from php to html include

---
+++ contact.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>CSS3_contrast</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <!-- modernizr enables HTML5 elements and feature detects -->
+  <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script> 
+  $(function(){
+    $("#header").load("header.html");
+    $("#side").load("side.html"); 
+    $("#footer").load("footer.html"); 
+  });
+  </script> 
+</head>
+
+<body>
+  <div id="main">
+    <div id="header"></div>
+    <div id="site_content">
+      <div id="side"></div>
+      <div class="content">
+        <h1>Contact Us</h1>
+        <p>Say hello or ask any question, using this contact form.</p>
+        <?php
+          // Set-up these 3 parameters
+          // 1. Enter the email address you would like the enquiry sent to
+          // 2. Enter the subject of the email you will receive, when someone contacts you
+          // 3. Enter the text that you would like the user to see once they submit the contact form
+          $to = 'johanna.tiemann@gmail.com';
+          $subject = 'Enquiry from the MDsrv';
+          $contact_submitted = 'Your message has been sent.';
+
+          // Do not amend anything below here, unless you know PHP
+          function email_is_valid($email) {
+            return preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i',$email);
+          }
+          if (!email_is_valid($to)) {
+            echo '<p style="color: red;">You must set-up a valid (to) email address before this contact page will work.</p>';
+          }
+          if (isset($_POST['contact_submitted'])) {
+            $return = "\r";
+            $youremail = trim(htmlspecialchars($_POST['your_email']));
+            $yourname = stripslashes(strip_tags($_POST['your_name']));
+            $yourmessage = stripslashes(strip_tags($_POST['your_message']));
+            $contact_name = "Name: ".$yourname;
+            $message_text = "Message: ".$yourmessage;
+            $user_answer = trim(htmlspecialchars($_POST['user_answer']));
+            $answer = trim(htmlspecialchars($_POST['answer']));
+            $message = $contact_name . $return . $message_text;
+            $headers = "From: ".$youremail;
+            if (email_is_valid($youremail) && !eregi("\r",$youremail) && !eregi("\n",$youremail) && $yourname != "" && $yourmessage != "" && substr(md5($user_answer),5,10) === $answer) {
+              mail($to,$subject,$message,$headers);
+              $yourname = '';
+              $youremail = '';
+              $yourmessage = '';
+              echo '<p style="color: blue;">'.$contact_submitted.'</p>';
+            }
+            else echo '<p style="color: red;">Please enter your name, a valid email address, your message and the answer to the simple maths question before sending your message.</p>';
+          }
+          $number_1 = rand(1, 9);
+          $number_2 = rand(1, 9);
+          $answer = substr(md5($number_1+$number_2),5,10);
+        ?>
+        <form id="contact" action="contact.php" method="post">
+          <div class="form_settings">
+            <p><span>Name</span><input class="contact" type="text" name="your_name" value="<?php echo $yourname; ?>" /></p>
+            <p><span>Email Address</span><input class="contact" type="text" name="your_email" value="<?php echo $youremail; ?>" /></p>
+            <p><span>Message</span><textarea class="contact textarea" rows="5" cols="50" name="your_message"><?php echo $yourmessage; ?></textarea></p>
+            <p style="line-height: 1.7em;">To help prevent spam, please enter the answer to this question:</p>
+            <p><span><?php echo $number_1; ?> + <?php echo $number_2; ?> = ?</span><input type="text" name="user_answer" /><input type="hidden" name="answer" value="<?php echo $answer; ?>" /></p>
+            <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="send" /></p>
+          </div>
+        </form>
+      </div>
+    </div>
+    
+<h1>
+<a id="more" class="anchor" href="#more" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>More</h1>
+
+<p>If you have question, feel free to use the
+<a target="_blank" href="https://github.com/arose/mdsrv/issues">Issue Tracker</a> or write a mail to
+<a href="mailto:johanna.tiemann@gmail.com">johanna.tiemann@gmail.com</a> or
+<a href="mailto:alexander.rose@weirdbyte.de">alexander.rose@weirdbyte.de</a>.</p>
+
+<p>Please give us <strong>feedback</strong>!</p>
+
+    <div id="scroll">
+      <a title="Scroll to the top" class="top" href="#"><img src="images/top.png" alt="top" /></a>
+    </div>
+  <div id="footer"></div>
+  </div>
+</body>
+</html>
