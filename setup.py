dee993cd873392d735130da7ad007fe7e17b9482
j0kaso <johanna.tiemann@gmail.com>
2017-08-03 08:36:53 +0200

    FIX: MDAnalysis python dependency (travis...)

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ setup_args = {
     'install_requires': {
         "flask": ["flask"],
         "mdtraj": ["mdtraj"],
-        #"simpletraj": ["simpletraj"],
+        "mdanalysis;python_version<'3.4';platform_system!='Windows'": ["mdanalysis"],
+        "simpletraj;platform_system!='Windows'": ["simpletraj"],
     },
     'packages': set(find_packages() + 
                 ['mdsrv']),
