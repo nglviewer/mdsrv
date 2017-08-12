4f632cc95a2023c9e73f66aba38837a8e82f1e27
j0kaso <johanna.tiemann@gmail.com>
2017-08-13 00:00:27 +0200

    UPDATE: testing script

--- develop/test.py
+++ develop/test.py
@@ -17,9 +17,29 @@ def main(argv):
     print(trajectory.importarray)
     
     print('Extratest: Import mdtraj')
-    import mdtraj
+    try:
+        import mdtraj
+        print('success')
+    except ImportError as e:
+        print('error',e)
 
+    print('Extratest: Import mdanalysis')
+    try:
+        import mdanalysis
+        print('success')
+    except ImportError as e:
+        print('error',e)
 
+    print('Extratest: mdtraj functionality')
+    try:
+        from mdtraj.formats import XTCTrajectoryFile
+        xtc = XTCTrajectoryFile( argv[0], 'r' )
+        xtc_traj = xtc.read(n_frames=1)
+        print('success:', xtc_traj)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message, ex)
 
 if __name__ == "__main__":
     main(argv=sys.argv[1:])
